ef918e96fe84f04aeb5baaaa9819f90c136e260a
Vivek Veeriah <vivekveeriah@gmail.com>
2021-12-13 12:05:33 -0500

    updated miscellaneous stuff about env

--- gym_miniworld/envs/tworoommod.py
+++ gym_miniworld/envs/tworoommod.py
@@ -31,7 +31,8 @@ class TwoRoomMod(MiniWorldEnv):
         self.possible_start_pos_x_room_2 = [
             -6.5, -6.0, -5.5, -5.0, -4.5, -4.0, -3.5, -3.0, -2.5, -2.0, -1.5, -1.0, -0.5,
             0., 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5]
-        self.possible_start_pos_z_room_2 = [-7.5, -7.0, -6.5, -6.0, -5.5, -5.0, -4.5, -4.0, -3.5, -3.0, -2.5, -2.0, -1.5, -1.0]
+        self.possible_start_pos_z_room_2 = [
+            -7.5, -7.0, -6.5, -6.0, -5.5, -5.0, -4.5, -4.0, -3.5, -3.0, -2.5, -2.0, -1.5, -1.0]
 
         self.possible_dir_radians = [i * math.pi / 180 for i in range(0, 370, 30)]
 
